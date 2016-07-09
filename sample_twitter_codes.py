2a74c5b7fafda69cfd8cb45f8ee8a64e4fc0ada6
Jake Goss-Kuehn <jakeinmn@gmail.com>
2016-07-09 11:30:05 -0500

    Add files via upload
    Added reasonable flow of events. Randomization. Stochastic limits.

--- sample_twitter_codes.py
+++ sample_twitter_codes.py
@@ -1,3 +1,6 @@
+'''
+CHECK TO SEE IF SCHOCASTIC VARIABLES TO BE SET CAN THROW OFF TWITTER...
+'''
 ''''
 In this code we'll have a bunch of examples you can use at your own discretion. 
 
@@ -15,32 +18,87 @@ Justin and Nat
 '''
 
 '''
-#1 Here you can automatically follow people who tweet about a certain phrase. Just replace the phrase
-with something relevant to you! Also you can set the count to whatever makes you most comfortable.
-
-from twitter_follow_bot import auto_follow
-auto_follow("phrase", count=100)
+#1 This code will automatically un-follow everyone who hasn't followed you back.
+You hit a limit with unfollowing... 200 a day.. double check to see if it even checks that...
+Makes much more sense to have this FIRST since you'd add people and then unfollow them right away
+lol
 '''
+from random import randint
+import random
+print("===========================================")
+print("Running the Script, c Jake GK 2016")
+print("")
+
+def randintWithHalfRandomness():
+	if randint(0,1) == 1:
+		return randint(3,9)
+	else:
+		return 0;
+		
+from twitter_follow_bot import auto_unfollow_nonfollowers
+print("Doing cool automated stuff so it frees up cool stuff for you to do better stuff!")
+print("===========================================")
+print("Now Unfollowing people who dont follow you with ~N(25,12) total")
+print("===========================================")
+#auto_unfollow_nonfollowers()
 
 '''
-#2 In this code, change "jwmares" to the twitter handle whose followers you want to follow, 
+#2 In this code, change "jakegosskuehn" to the twitter handle whose followers you want to follow, 
 and set the count to how many people should be followed. Default is 100.
 
+Good follow up would be to get a list of your most popular followers, and follow from THEM.
+'''
+print("===========================================")
+print("Now Following people who follow you! ~N(25,12) total")
+print("===========================================")
 from twitter_follow_bot import auto_follow_followers_for_user
-auto_follow_followers_for_user("jwmares", count=10)
-'''
+auto_follow_followers_for_user("jakegosskuehn", count=100)
+
+
+
 
 '''
-#3 This code will let you favoite things that are relevant to you. Just replace "phrase" with the phrase
-you want to favorite for, and set the count to how many things you want to favorite.
+#3 Here you can automatically follow people who tweet about a certain phrase. Just replace the phrase
+with something relevant to you! Also you can set the count to whatever makes you most comfortable.
+'''
 
+
+
+print("===========================================")
+print("Now following those who like your hashtags! ~B(.5)*U(3,9)")
+print("===========================================")
+from twitter_follow_bot import auto_follow
 from twitter_follow_bot import auto_fav
-auto_fav("phrase", count=100)
-'''
+auto_fav("socialmediamarketing", count=randint(0,1))
+auto_follow("socialmediamarketing", count=randintWithHalfRandomness())
+auto_fav("softwaredevelopment", count=randint(0,1))
+auto_follow("softwaredevelopment", count=randintWithHalfRandomness())
+auto_fav("startup", count=randint(0,1))
+auto_follow("startup", count=randintWithHalfRandomness())
+auto_fav("smm", count=randint(0,1))
+auto_follow("smm", count=randintWithHalfRandomness())
+auto_fav("homebrew", count=randint(0,1))
+auto_follow("homebrew", count=randintWithHalfRandomness())
+auto_fav("nomad", count=randint(0,1))
+auto_follow("nomad", count=randintWithHalfRandomness())
+
 
 '''
-#4 This code will automatically un-follow everyone who hasn't followed you back.
-
-from twitter_follow_bot import auto_unfollow_nonfollowers
-auto_unfollow_nonfollowers()
+#4 This code will let you favoite things that are relevant to you. Just replace "phrase" with the phrase
+you want to favorite for, and set the count to how many things you want to favorite.
 '''
+#from twitter_follow_bot import auto_fav
+#auto_fav("socialmediamarketing", count=1)
+#auto_fav("nomadlife", count=1)
+#auto_fav("homebrewweekend", count=1)
+#auto_fav("socialmediamanager", count=1)
+#auto_fav("startupculture", count=1)
+#auto_fav("rollotomassi", count=1)
+#auto_fav("betabucksalphafucks", count=1)
+#auto_fav("pussypass", count=1)
+#auto_fav("cherrybit", count=1)
+
+
+print("===========================================")
+print("PYTHON BOT IS DONE. I WILL SLEEP UNTIL CALLED.")
+print("===========================================")
--- twitter_follow_bot.py
+++ twitter_follow_bot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright 2014 Randal S. Olson
+Copyright 2014 Randal S. Olson, Jake Goss-Kuehn
 
 This file is part of the Twitter Follow Bot library.
 
@@ -23,223 +23,281 @@ storage.
 
 from twitter import Twitter, OAuth, TwitterHTTPError
 import os
+import time
+import random
+from random import randint
 from twitter_info import *
 
 # put the full path and file name of the file you want to store your "already followed"
 # list in
+#sleep = random.random() * 7 # 0 to 7 seconds
+#Good to mimic the 3 twitter followers that you get on the side
+#Every 3rd have a PAUSE... hmmm
+
+def setGlobals():
+	global thirdcount
+	thirdcount = 0
+	global sessionfollows
+	sessionfollows = 0
+	global sessionunfollows
+	sessionunfollows = 0
+	
+
+
 ALREADY_FOLLOWED_FILE = "already-followed.csv"
 
-t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,
-            CONSUMER_KEY, CONSUMER_SECRET))
+t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET,CONSUMER_KEY, CONSUMER_SECRET))
 
 
 def search_tweets(q, count=100, result_type="recent"):
-    """
-        Returns a list of tweets matching a certain phrase (hashtag, word, etc.)
-    """
+	'''
+		Returns a list of tweets matching a certain phrase (hashtag, word, etc.)
+	'''
+	return t.search.tweets(q=q, result_type=result_type, count=count)
 
-    return t.search.tweets(q=q, result_type=result_type, count=count)
-
-
+#After this auto_fav follows just ONE favorite, it breaks out of the method's for loop and moves on with its life. Maybe it finds a nice cottage out in the mountains, maybe it just finds a nice suburb to raise a middle class family. Whatever it does, it stops what it is doing to continue on. It does this once if anything, and stops.
 def auto_fav(q, count=100, result_type="recent"):
-    """
-        Favorites tweets that match a certain phrase (hashtag, word, etc.)
-    """
+	"""
+		Favorites tweets that match a certain phrase (hashtag, word, etc.)
+	"""
 
-    result = search_tweets(q, count, result_type)
+	result = search_tweets(q, count, result_type)
 
-    for tweet in result["statuses"]:
-        try:
-            # don't favorite your own tweets
-            if tweet["user"]["screen_name"] == TWITTER_HANDLE:
-                continue
-
-            result = t.favorites.create(_id=tweet["id"])
-            print("favorited: %s" % (result["text"].encode("utf-8")))
-
-        # when you have already favorited a tweet, this error is thrown
-        except TwitterHTTPError as e:
-            print("error: %s" % (str(e)))
+	for tweet in result["statuses"]:
+		
+		try:
+			# don't favorite your own tweets
+			if tweet["user"]["screen_name"] == TWITTER_HANDLE:
+				continue
+			time.sleep(randint(5,13))
+			result = t.favorites.create(_id=tweet["id"])
+			print("favorited on %s : %s " % (q, result["text"].encode("utf-8")))
+			break
+		# when you have already favorited a tweet, this error is thrown
+		except TwitterHTTPError as e:
+			print("error: %s" % (str(e)))
 
 
 def auto_rt(q, count=100, result_type="recent"):
-    """
-        Retweets tweets that match a certain phrase (hashtag, word, etc.)
-    """
+	"""
+		Retweets tweets that match a certain phrase (hashtag, word, etc.)
+	"""
 
-    result = search_tweets(q, count, result_type)
+	result = search_tweets(q, count, result_type)
 
-    for tweet in result["statuses"]:
-        try:
-            # don't retweet your own tweets
-            if tweet["user"]["screen_name"] == TWITTER_HANDLE:
-                continue
+	for tweet in result["statuses"]:
+		sleep(random.random() * 10 + 3)
+		try:
+			# don't retweet your own tweets
+			if tweet["user"]["screen_name"] == TWITTER_HANDLE:
+				continue
 
-            result = t.statuses.retweet(id=tweet["id"])
-            print("retweeted: %s" % (result["text"].encode("utf-8")))
+			result = t.statuses.retweet(id=tweet["id"])
+			print("retweeted: %s" % (result["text"].encode("utf-8")))
 
-        # when you have already retweeted a tweet, this error is thrown
-        except TwitterHTTPError as e:
-            print("error: %s" % (str(e)))
+		# when you have already retweeted a tweet, this error is thrown
+		except TwitterHTTPError as e:
+			print("error: %s" % (str(e)))
 
 
 def get_do_not_follow_list():
-    """
-        Returns list of users the bot has already followed.
-    """
+	"""
+		Returns list of users the bot has already followed.
+	"""
 
-    # make sure the "already followed" file exists
-    if not os.path.isfile(ALREADY_FOLLOWED_FILE):
-        with open(ALREADY_FOLLOWED_FILE, "w") as out_file:
-            out_file.write("")
+	# make sure the "already followed" file exists
+	if not os.path.isfile(ALREADY_FOLLOWED_FILE):
+		with open(ALREADY_FOLLOWED_FILE, "w") as out_file:
+			out_file.write("")
 
-        # read in the list of user IDs that the bot has already followed in the
-        # past
-    do_not_follow = set()
-    dnf_list = []
-    with open(ALREADY_FOLLOWED_FILE) as in_file:
-        for line in in_file:
-            dnf_list.append(int(line))
+		# read in the list of user IDs that the bot has already followed in the
+		# past
+	do_not_follow = set()
+	dnf_list = []
+	with open(ALREADY_FOLLOWED_FILE) as in_file:
+		for line in in_file:
+			dnf_list.append(int(line))
 
-    do_not_follow.update(set(dnf_list))
-    del dnf_list
+	do_not_follow.update(set(dnf_list))
+	del dnf_list
 
-    return do_not_follow
+	return do_not_follow
 
 
 def auto_follow(q, count=100, result_type="recent"):
-    """
-        Follows anyone who tweets about a specific phrase (hashtag, word, etc.)
-    """
+	"""
+		Follows anyone who tweets about a specific phrase (hashtag, word, etc.)
+		Sleeps in intervals of THREES to play against twitter
+	"""
 
-    result = search_tweets(q, count, result_type)
-    following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    do_not_follow = get_do_not_follow_list()
+	#Fucking sucks. Globals cansuck my left nut
+	global sessionfollows
+	sessionfollows = 0
+	global thirdcount
+	thirdcount = 0
+	global followlimit
+	#followlimit = random.random() * 75 + 150
+	followlimit = random.random() * 15 + 10
+	
+	result = search_tweets(q, count, result_type)
+	following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
+	do_not_follow = get_do_not_follow_list()
 
-    for tweet in result["statuses"]:
-        try:
-            if (tweet["user"]["screen_name"] != TWITTER_HANDLE and
-                    tweet["user"]["id"] not in following and
-                    tweet["user"]["id"] not in do_not_follow):
+	for tweet in result["statuses"]:
+		sessionfollows = sessionfollows + 1
+		if sessionfollows > followlimit:
+			continue
+		elif thirdcount == 3:
+			thirdcount = 0
+			time.sleep(random.random() * 12 + 3)
+		
+		try:
+			
+			thirdcount = thirdcount + 1
+			if (tweet["user"]["screen_name"] != TWITTER_HANDLE and 
+			tweet["user"]["id"] not in following and 
+			tweet["user"]["id"] not in do_not_follow):
+				time.sleep(3)
+				t.friendships.create(user_id=tweet["user"]["id"], follow=False)
+				following.update(set([tweet["user"]["id"]]))
 
-                t.friendships.create(user_id=tweet["user"]["id"], follow=False)
-                following.update(set([tweet["user"]["id"]]))
+				print("followed %s on %s %d / %d left" % (tweet["user"]["screen_name"], q, sessionfollows, followlimit))
 
-                print("followed %s" % (tweet["user"]["screen_name"]))
+		except TwitterHTTPError as e:
+			print("error: %s" % (str(e)))
 
-        except TwitterHTTPError as e:
-            print("error: %s" % (str(e)))
-
-            # quit on error unless it's because someone blocked me
-            if "blocked" not in str(e).lower():
-                quit()
+			# quit on error unless it's because someone blocked me
+			if "blocked" not in str(e).lower():
+				quit()
 
 
 def auto_follow_followers_for_user(user_screen_name, count=100):
-    """
-        Follows the followers of a user
-    """
-    following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    followers_for_user = set(t.followers.ids(screen_name=user_screen_name)["ids"][:count]);
-    do_not_follow = get_do_not_follow_list()
-    
-    for user_id in followers_for_user:
-        try:
-            if (user_id not in following and 
-                user_id not in do_not_follow):
+	"""
+		Follows the followers of a user
+	"""
+	global sum
+	sum = 0
+	global sessionfollows
+	sessionfollows = 0
+	following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
+	followers_for_user = set(t.followers.ids(screen_name=user_screen_name)["ids"][:count]);
+	do_not_follow = get_do_not_follow_list()
 
-                t.friendships.create(user_id=user_id, follow=False)
-                print("followed %s" % user_id)
+	for user_id in followers_for_user:
+		sum = sum + 1
+		sessionfollows = sessionfollows + 1
+		if sessionfollows > random.random() * 25 + 75:
+			break
 
-        except TwitterHTTPError as e:
-            print("error: %s" % (str(e)))
+		try:
+			if (user_id not in following and 
+				user_id not in do_not_follow):
+				time.sleep(random.random() * 16 + 6)
+				t.friendships.create(user_id=user_id, follow=False)
+				print("mutually followed %s %d/%d left" % (user_id, sum, len(followers_for_user)))
+
+		except TwitterHTTPError as e:
+			print("error: %s" % (str(e)))
 
 def auto_follow_followers():
-    """
-        Follows back everyone who's followed you
-    """
+	"""
+		Follows back everyone who's followed you
+	"""
 
-    following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    followers = set(t.followers.ids(screen_name=TWITTER_HANDLE)["ids"])
+	following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
+	followers = set(t.followers.ids(screen_name=TWITTER_HANDLE)["ids"])
 
-    not_following_back = followers - following
+	not_following_back = followers - following
 
-    for user_id in not_following_back:
-        try:
-            t.friendships.create(user_id=user_id, follow=False)
-        except Exception as e:
-            print("error: %s" % (str(e)))
+	for user_id in not_following_back:
+		if sessionfollows > random.random() * 75 + 150:
+			continue
+		try:
+			t.friendships.create(user_id=user_id, follow=False)
+			sessionfollows = sessionfollows + 1
+		except Exception as e:
+			print("error: %s" % (str(e)))
 
 
 def auto_unfollow_nonfollowers():
-    """
-        Unfollows everyone who hasn't followed you back
-    """
+	"""
+		Unfollows everyone who hasn't followed you back
+		Needs to be able to not always reach the 200... a day...
+		Also needs to start at RANDOM times in the morning
+		to mimic REAL useage as not to be banned...
+		Doesnt have a limit... probally breaks at any time.
+	"""
+	global sessionunfollows
+	sessionunfollows = 0
+	unfollowlimit = random.random() * 25 + 125
+	following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
+	followers = set(t.followers.ids(screen_name=TWITTER_HANDLE)["ids"])
 
-    following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    followers = set(t.followers.ids(screen_name=TWITTER_HANDLE)["ids"])
+	# put user IDs here that you want to keep following even if they don't
+	# follow you back
+	users_keep_following = set([])
 
-    # put user IDs here that you want to keep following even if they don't
-    # follow you back
-    users_keep_following = set([])
-
-    not_following_back = following - followers
+	not_following_back = following - followers
 
     # make sure the "already followed" file exists
-    if not os.path.isfile(ALREADY_FOLLOWED_FILE):
-        with open(ALREADY_FOLLOWED_FILE, "w") as out_file:
-            out_file.write("")
+	if not os.path.isfile(ALREADY_FOLLOWED_FILE):
+		with open(ALREADY_FOLLOWED_FILE, "w") as out_file:
+			out_file.write("")
 
-    # update the "already followed" file with users who didn't follow back
-    already_followed = set(not_following_back)
-    af_list = []
-    with open(ALREADY_FOLLOWED_FILE) as in_file:
-        for line in in_file:
-            af_list.append(int(line))
+	# update the "already followed" file with users who didn't follow back
+	already_followed = set(not_following_back)
+	af_list = []
+	with open(ALREADY_FOLLOWED_FILE) as in_file:
+		for line in in_file:
+			af_list.append(int(line))
 
-    already_followed.update(set(af_list))
-    del af_list
+	already_followed.update(set(af_list))
+	del af_list
 
-    with open(ALREADY_FOLLOWED_FILE, "w") as out_file:
-        for val in already_followed:
-            out_file.write(str(val) + "\n")
+	with open(ALREADY_FOLLOWED_FILE, "w") as out_file:
+		for val in already_followed:
+			out_file.write(str(val) + "\n")
 
-    for user_id in not_following_back:
-        if user_id not in users_keep_following:
-            t.friendships.destroy(user_id=user_id)
-            print("unfollowed %d" % (user_id))
+	for user_id in not_following_back:
+		if sessionunfollows > unfollowlimit:
+			continue
+		if user_id not in users_keep_following:
+			time.sleep(random.random() * 4 + 10)
+			t.friendships.destroy(user_id=user_id)
+			sessionunfollows = sessionunfollows + 1
+			print("unfollowed %d %d/%d left" % (user_id, sessionunfollows, unfollowlimit))
 
 
 def auto_mute_following():
-    """
-        Mutes everyone that you are following
-    """
-    following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
-    muted = set(t.mutes.users.ids(screen_name=TWITTER_HANDLE)["ids"])
+	"""
+		Mutes everyone that you are following
+	"""
+	following = set(t.friends.ids(screen_name=TWITTER_HANDLE)["ids"])
+	muted = set(t.mutes.users.ids(screen_name=TWITTER_HANDLE)["ids"])
 
-    not_muted = following - muted
+	not_muted = following - muted
 
-    # put user IDs of people you do not want to mute here
-    users_keep_unmuted = set([])
+	# put user IDs of people you do not want to mute here
+	users_keep_unmuted = set([])
             
-    # mute all        
-    for user_id in not_muted:
-        if user_id not in users_keep_unmuted:
-            t.mutes.users.create(user_id=user_id)
-            print("muted %d" % (user_id))
+	# mute all        
+	for user_id in not_muted:
+		if user_id not in users_keep_unmuted:
+			t.mutes.users.create(user_id=user_id)
+			print("muted %d" % (user_id))
 
 
 def auto_unmute():
-    """
-        Unmutes everyone that you have muted
-    """
-    muted = set(t.mutes.users.ids(screen_name=TWITTER_HANDLE)["ids"])
+	"""
+		Unmutes everyone that you have muted
+	"""
+	muted = set(t.mutes.users.ids(screen_name=TWITTER_HANDLE)["ids"])
 
-    # put user IDs of people you want to remain muted here
-    users_keep_muted = set([])
-            
-    # mute all        
-    for user_id in muted:
-        if user_id not in users_keep_muted:
-            t.mutes.users.destroy(user_id=user_id)
-            print("unmuted %d" % (user_id))
+	# put user IDs of people you want to remain muted here
+	users_keep_muted = set([])
+
+	# mute all        
+	for user_id in muted:
+		if user_id not in users_keep_muted:
+			t.mutes.users.destroy(user_id=user_id)
+			print("unmuted %d" % (user_id))
